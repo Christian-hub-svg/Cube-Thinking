b5f75344bd75f33b2c1d01d4d88fbb7f42c3b822
Christian Hitoshi <tetihamai@gmailcom>
2021-05-13 21:26:14 -0300

    quiz finaalizado

---
+++ site/js/quiz.js
@@ -0,0 +1,250 @@
+var pontos = 0;
+
+
+//pergunta 1
+function btn1(){
+    id_btn1.style.background = `#f30b0bb6`;
+    id_btn2.disabled = true;
+    id_btn3.disabled = true;
+    id_btn4.style.background = `#45d80bb6`;
+
+    pontos += 0;
+}
+function btn2(){
+    id_btn1.disabled = true;
+    id_btn2.style.background = `#f30b0bb6`;
+    id_btn3.disabled = true;
+    id_btn4.style.background = `#45d80bb6`;
+
+    pontos += 0;
+}
+function btn3(){
+    id_btn1.disabled = true;
+    id_btn2.disabled = true;
+    id_btn3.style.background = `#f30b0bb6`;
+    id_btn4.style.background = `#45d80bb6`;
+
+    pontos += 0;
+}
+function btn4(){
+    id_btn1.disabled = true;
+    id_btn2.disabled = true;
+    id_btn3.disabled = true;
+    id_btn4.style.background = `#45d80bb6`;
+
+    pontos += 1;
+}
+
+
+//pergunta 2
+function btn5(){
+    id_btn5.style.background = '#f30b0bb6';
+    id_btn6.disabled = true;
+    id_btn7.disabled = true;
+    id_btn8.style.background = `#45d80bb6`;
+
+    pontos += 0;
+}
+function btn6(){
+    id_btn5.disabled = true;
+    id_btn6.style.background = '#f30b0bb6';
+    id_btn7.disabled = true;
+    id_btn8.style.background = `#45d80bb6`;
+
+    pontos += 0;
+}
+function btn7(){
+    id_btn5.disabled = true;
+    id_btn6.disabled = true;
+    id_btn7.style.background = '#f30b0bb6';
+    id_btn8.style.background = `#45d80bb6`;
+
+    pontos += 0;
+}
+function btn8(){
+    id_btn5.disabled = true;
+    id_btn6.disabled = true;
+    id_btn7.disabled = true;
+    id_btn8.style.background = `#45d80bb6`;
+
+    pontos += 2;
+}
+
+
+//pergunta 3
+function btn9(){
+    id_btn9.style.background = '#f30b0bb6';
+    id_btn10.style.background = `#45d80bb6`;
+    id_btn11.disabled = true;
+    id_btn12.disabled = true;
+
+    pontos += 0;
+}
+function btn10(){
+    id_btn9.disabled = true;
+    id_btn10.style.background = `#45d80bb6`;
+    id_btn11.disabled = true;
+    id_btn12.disabled = true;
+
+    pontos += 1;
+}
+function btn11(){
+    id_btn9.disabled = true;
+    id_btn10.style.background = `#45d80bb6`;
+    id_btn11.style.background = '#f30b0bb6';
+    id_btn12.disabled = true;
+
+    pontos += 0;
+}
+function btn12(){
+    id_btn9.disabled = true;
+    id_btn10.style.background = `#45d80bb6`;
+    id_btn11.disabled = true;
+    id_btn12.style.background = '#f30b0bb6';
+
+    pontos += 0;
+}
+
+//pergunta 4
+function btn13(){
+    id_btn13.style.background = `#45d80bb6`;
+    id_btn14.disabled = true;
+    id_btn15.disabled = true;
+    id_btn16.disabled = true;
+
+    pontos += 3;
+}
+function btn14(){
+    id_btn13.style.background = `#45d80bb6`;
+    id_btn14.style.background = '#f30b0bb6';
+    id_btn15.disabled = true;
+    id_btn16.disabled = true;
+
+    pontos += 0;
+}
+function btn15(){
+    id_btn13.style.background = `#45d80bb6`;
+    id_btn14.disabled = true;
+    id_btn15.style.background = '#f30b0bb6';
+    id_btn16.disabled = true;
+
+    pontos += 0;
+}
+function btn16(){
+    id_btn13.style.background = `#45d80bb6`;
+    id_btn14.disabled = true;
+    id_btn15.disabled = true;
+    id_btn16.style.background = '#f30b0bb6';
+
+    pontos += 0;
+}
+
+
+//pergunta 5
+function btn17(){
+    id_btn17.style.background = '#f30b0bb6';
+    id_btn18.disabled = true;
+    id_btn19.style.background = `#45d80bb6`;
+    id_btn20.disabled = true;
+
+    pontos += 0;
+}
+function btn18(){
+    id_btn17.disabled = true;
+    id_btn18.style.background = '#f30b0bb6';
+    id_btn19.style.background = `#45d80bb6`;
+    id_btn20.disabled = true;
+
+    pontos += 0;
+}
+function btn19(){
+    id_btn17.disabled = true;
+    id_btn18.disabled = true;
+    id_btn19.style.background = `#45d80bb6`;
+    id_btn20.disabled = true;
+
+    pontos += 1;
+}
+function btn20(){
+    id_btn17.disabled = true;
+    id_btn18.disabled = true;
+    id_btn19.style.background = `#45d80bb6`;
+    id_btn20.style.background = '#f30b0bb6';
+
+    pontos += 0;
+}
+
+
+//pergunta 6
+function btn21(){
+    id_btn21.style.background = `#45d80bb6`;
+    id_btn22.disabled = true;
+    id_btn23.disabled = true;
+    id_btn24.disabled = true;
+
+    pontos += 2;
+}
+function btn22(){
+    id_btn21.style.background = `#45d80bb6`;
+    id_btn22.style.background = '#f30b0bb6';
+    id_btn23.disabled = true;
+    id_btn24.disabled = true;
+
+    pontos += 0;
+}
+function btn23(){
+    id_btn21.style.background = `#45d80bb6`;
+    id_btn22.disabled = true;
+    id_btn23.style.background = '#f30b0bb6';
+    id_btn24.disabled = true;
+
+    pontos += 0;
+}
+function btn24(){
+    id_btn21.style.background = `#45d80bb6`;
+    id_btn22.disabled = true;
+    id_btn23.disabled = true;
+    id_btn24.style.background = '#f30b0bb6';
+
+    pontos += 0;
+}
+
+
+
+function finalizar(){
+
+    var nome = id_nome.value;
+
+    if(pontos <= 3){
+        var imagem = `https://media.giphy.com/media/3o6wrebnKWmvx4ZBio/giphy.gif`;
+        var mensagem = `Podemos dizer que você é a vergonha da profissão?<br>
+        Acho que não, apenas que você não conhece muito desse mundo...<br>
+        Mas não desanime! Experimente se arofundar nesse incrível mundo do cubo mágico!`;
+    } else if(pontos <= 6){
+        var imagem = `https://media.giphy.com/media/VfKNGDf6F1PftXsHPy/giphy.gif`;
+        var mensagem = `Você foi bem, mas poderia ir melhor.<br>
+        Provavelmente você já sabe resolver o basico do cubo mágico, ou só chutou mesmo.<br>
+        Por que não experimenta entrar mais profundamente nesse mundo? Tenho certeza que irá amar!`;
+    } else if(pontos <= 9){
+        var imagem = `https://media.giphy.com/media/3NtY188QaxDdC/giphy.gif`;
+        var mensagem = `Você arrasou demais! Fiquei impressionado.<br>
+        Você com certeza sabe montar alguns tipos de cubo, e adora um desafio...<br>
+        Você deveria treinar para campeonatos, vai que um novo recorde vem de você...`;
+    } else {
+        var imagem = `https://media.giphy.com/media/U4DswrBiaz0p67ZweH/giphy.gif`;
+        var mensagem = `Não acredito que você conseguiu gabaritar isso...<br>
+        realmente você superou todas as expectativas e é um grande mestre nisso!<br>
+        Preciso de algumas dicas com você, porque não se cadastra e entra em contato comigo?`;
+    }
+    
+
+    id_mens_final.innerHTML =
+    `<h1 style="font-weight: 300;">${nome}, você fez ${pontos} pontos!</h1><br>
+    <img src="${imagem}" style="height: 20vh">
+    
+    <h3 style="font-weight: 300;">${mensagem}</h3>
+    `;
+
+}
+
+
